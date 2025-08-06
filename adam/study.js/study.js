795b336ad9c543885eacdb3d1aa8a08cf5b7cea9
D3mon-Romulus <134565976+D3mon-Romulus@users.noreply.github.com>
2025-08-06 18:42:22 +1000

    Add files via upload

---
+++ adam/study.js/study.js
@@ -0,0 +1,20 @@
+//console.log('hello');
+//window.alert('hello world');
+document.getElementById("heyYou").textContent = "hello world";
+//document.getElementById("heyYou").style.color = "red";
+
+	//let fullName = "John Doe";
+	//let age = 30;
+	//let isStudent = false;
+
+   //document.getElementById("p1").textContent =`Your Name is ${fullName}`;
+   //document.getElementById("p2").textContent =`Your Age is ${age}`;  
+   //document.getElementById("p3").textContent =`Are you a student? ${isStudent}`;
+
+   // let x;
+    //let y;
+    //x = 5;
+    //y = 10;
+    //console.log(x + y); // Output: 15
+    let students = 30;
+    
